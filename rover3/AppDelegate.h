fe703aec2e6080befeba4df40014d180a97ee204
Jesse Chen <jesseyuanchen@college.harvard.edu>
2012-10-13 02:20:42 -0400

    initial push

---
+++ rover3/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  rover3
+//
+//  Created by Jesse Chen on 10/13/12.
+//  Copyright (c) 2012 Jesse Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ rover3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  rover3
+//
+//  Created by Jesse Chen on 10/13/12.
+//  Copyright (c) 2012 Jesse Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
